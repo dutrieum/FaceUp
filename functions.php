33325c8442a02c65ff3ad27553e0571fd9ad9451
dutrieum <dutrieuxmathilde@gmail.com>
2020-11-16 19:46:36 +0100

    Onglet Header

--- functions.php
+++ functions.php
@@ -9,7 +9,18 @@
             'icon_url' => 'dashicons-buddicons-groups'
         ));
   }
-  
+
+  if(function_exists('acf_add_options_page')) {
+    acf_add_options_page(array(
+            'page_title' => 'Header',
+            'menu_title' => 'Header',
+            'menu_slug' => 'header',
+            'capability' => 'read',
+            'redirect' => false,
+            'icon_url' => 'dashicons-buddicons-groups'
+        ));
+  }
+
   function load_files() {
     wp_register_style( 'footer', get_template_directory_uri() . 'assets/css/footer.css', array(), '1.1', 'all');
     wp_enqueue_style( 'footer');
