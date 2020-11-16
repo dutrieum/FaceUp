4ca6c05a935b5d6947a5436d6cf7da0cc05eedff
vinsonj <julievnsn@gmail.com>
2020-11-16 17:51:16 +0100

    footer

--- footer.php
+++ footer.php
@@ -1,2 +1,12 @@
+<footer>
+  <link type="text/css" rel="stylesheet" href="assets/css/footer.css"/>
+
+
 <h2><?php the_field('address', 'option'); ?></h2>
 <h2><?php the_field('phone', 'option'); ?></h2>
+<h2><?php the_field('email', 'option'); ?></h2>
+<h2><?php $mentions = get_field('mentions', 'option');?></h2>
+
+<a href="<?php echo(get_permalink($mentions->ID)); ?>">Mentions Légales</a>
+
+</footer>
