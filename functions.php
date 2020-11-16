efe7d0a53cf8e280a3a91828aeb7df44c6031367
dutrieum <dutrieuxmathilde@gmail.com>
2020-11-16 22:03:27 +0100

    modif header

--- functions.php
+++ functions.php
@@ -23,8 +23,9 @@
 
   function load_files() {
     wp_enqueue_style( 'style', get_stylesheet_uri() );
+    wp_enqueue_style( 'reset', get_template_directory_uri() . '/assets/css/reset.css', array(), '1.1');
     wp_enqueue_style( 'footer', get_template_directory_uri() . '/assets/css/footer.css', array(), '1.1');
     wp_enqueue_style( 'header', get_template_directory_uri() . '/assets/css/header.css', array(), '1.1');
   }
-  add_action( 'wp_enqueue_style', 'load_files' );
+  add_action( 'wp_enqueue_scripts', 'load_files' );
 ?>
--- header.php
+++ header.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html>
+<?php
+  wp_head();
+?>
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
   </head>
   <body>
     <header>
-      <a href="<?php echo home_url( '/' ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.svg" alt="Website logo"></img></a>
+      <a href="<?php echo home_url( '/' ); ?>"><img class="logotype" src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.svg" alt="Website logo"></img></a>
     </header>
