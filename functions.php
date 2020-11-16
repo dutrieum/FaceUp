c381366d586e338a72c4cd5298353041c598ec00
dutrieum <dutrieuxmathilde@gmail.com>
2020-11-16 18:31:14 +0100

    modif add_theme

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@
   function add_theme_scripts() {
     wp_enqueue_style( 'style', get_stylesheet_uri() );
 
-    wp_enqueue_style( 'slider', get_template_directory_uri() . 'assets/css/footer.css', array(), '1.1', 'all');
+    wp_enqueue_style( 'footer', get_template_directory_uri() . 'assets/css/footer.css', array(), '1.1', 'all');
 
   }
   add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
