10651526c251c4586eac55553bc79e131a6ec656
vinsonj <julievnsn@gmail.com>
2020-11-16 19:25:19 +0100

    changement

--- footer.php
+++ footer.php
@@ -6,7 +6,9 @@
 <h2><?php the_field('phone', 'option'); ?></h2>
 <h2><?php the_field('email', 'option'); ?></h2>
 <h2><?php $mentions = get_field('mentions', 'option');?></h2>
-
 <a href="<?php echo(get_permalink($mentions->ID)); ?>">Mentions Légales</a>
+
+<h2><?php $button = get_field('button', 'option');?></h2>
+<a href="<?php echo(get_permalink($button->ID)); ?>">Prendre rendez-vous</a>
 
 </footer>
