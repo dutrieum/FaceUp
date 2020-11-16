4a915629591d8ee43d3279e53de611fc2d45e68e
dutrieum <dutrieuxmathilde@gmail.com>
2020-11-16 18:21:18 +0100

    lien avec le footer.css

--- functions.php
+++ functions.php
@@ -9,4 +9,11 @@
             'icon_url' => 'dashicons-buddicons-groups'
         ));
       }
+  function add_theme_scripts() {
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+    wp_enqueue_style( 'slider', get_template_directory_uri() . 'assets/css/footer.css', array(), '1.1', 'all');
+
+  }
+  add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 ?>
