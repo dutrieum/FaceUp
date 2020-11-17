1b6d025007dba3d0b0b402ce9e74895b1f4f7d80
melissande <melissandec@gmail.com>
2020-11-17 15:50:55 +0100

    presentation.php & presentation.css

---
+++ templates/presentation.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Template Name: presentation
+*/
+?>
+
+<?php
+get_header();
+wp_head();
+?>
+
+<presentation>
+  <div class="wrap">
+    <div class="photo">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/claire.png" alt="">
+    </div>
+    <div class="text">
+      <?php the_field('title1')
+    </div>
+
+
+  </div>
+
+
+</presentation>
+
+
+<?php
+  get_footer();
+  wp_footer();
+?>
