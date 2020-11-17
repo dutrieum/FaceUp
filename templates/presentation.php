19ba8d372ab48850faad52a0d8cf107aaeb2f2f5
melissande <melissandec@gmail.com>
2020-11-17 15:53:46 +0100

    presentation.php

--- templates/presentation.php
+++ templates/presentation.php
@@ -15,7 +15,7 @@ wp_head();
       <img src="<?php echo get_template_directory_uri(); ?>/assets/img/claire.png" alt="">
     </div>
     <div class="text">
-      <?php the_field('title1')
+      <?php the_field('title1') ?>
     </div>
 
 
