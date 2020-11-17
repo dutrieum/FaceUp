50a06a099620eaa8fe48bd3421f231b843744b75
vinsonj <julievnsn@gmail.com>
2020-11-17 14:06:39 +0100

    modif functions

--- functions.php
+++ functions.php
@@ -15,6 +15,8 @@
     wp_enqueue_style( 'reset', get_template_directory_uri() . '/assets/css/reset.css', array(), '1.1');
     wp_enqueue_style( 'footer', get_template_directory_uri() . '/assets/css/footer.css', array(), '1.1');
     wp_enqueue_style( 'header', get_template_directory_uri() . '/assets/css/header.css', array(), '1.1');
+    wp_enqueue_style( 'portrait', get_template_directory_uri() . '/assets/css/portrait.css', array(), '1.1');
+    wp_enqueue_style( 'seances', get_template_directory_uri() . '/assets/css/seances.css', array(), '1.1');
   }
   add_action( 'wp_enqueue_scripts', 'load_files' );
 
