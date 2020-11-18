71c55027b2ab678fb08482d3c515cd10d42fd326
vinsonj <julievnsn@gmail.com>
2020-11-18 10:04:42 +0100

    modif nom var

--- templates/portait.php
+++ templates/portait.php
@@ -8,26 +8,26 @@
   get_header();
 ?>
 
-<div class="bloc1">
-  <div class="presentation">
-    <div id="titre">
+<div class="bloc1_portrait">
+  <div class="presentation_portrait">
+    <div id="title_portrait">
       <p><?php the_field('portrait');?></p>
     </div>
 
-    <div class="photo">
+    <div class="photo_portrait">
       <img src="<?php echo get_template_directory_uri(); ?>/assets/img/claire.png" alt="Photo Claire">
     </div>
 
-    <div class="texte">
-      <div id="texte1">
+    <div class="text_portrait">
+      <div id="text1_portrait">
         <p><?php the_field('claire'); ?></p>
       </div>
 
-      <div id="texte2">
+      <div id="text2_portrait">
         <p><?php the_field('subtitle'); ?></p>
       </div>
 
-      <div id="texte3">
+      <div id="text3_portrait">
         <p><?php the_field('description1'); ?></p>
       </div>
 
