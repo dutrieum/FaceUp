273da94923ff2355304e298013f999bcdc1746ca
melissande <melissandec@gmail.com>
2020-11-18 10:20:40 +0100

    .

--- templates/presentation.php
+++ templates/presentation.php
@@ -9,17 +9,17 @@ get_header();
 ?>
 
 <presentation>
-  <div class="wrap">
-    <div class="photo">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/osteopathe.png" alt="ostéopathe">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/massage.png" alt="massage">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/visage.png" alt="visage">
+  <div class="wrap_presentation">
+    <div class="photo_presentation">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/osteopathe_presentatipon.png" alt="ostéopathe">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/massage_presentatipon.png" alt="massage">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/visage_presentatipon.png" alt="visage">
     </div>
 <div>
   <h1><?php the_field('presentation'); ?></h1>
 </div>
 
-    <div class="title">
+    <div class="title_presentation">
       <div>
       <p><?php the_field('title1'); ?></p>
     </div>
@@ -33,7 +33,7 @@ get_header();
 </div>
     </div>
 
-<div class="text">
+<div class="text_presentation">
 
   <div>
   <p><?php the_field('facialist'); ?></p>
