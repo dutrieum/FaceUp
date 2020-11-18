2a2ce3a8efe084066a1c5e217bc75ea392298479
melissande <melissandec@gmail.com>
2020-11-18 17:14:06 +0100

    presentation.css

--- templates/presentation.php
+++ templates/presentation.php
@@ -16,19 +16,19 @@ get_header();
 
 <div class="wrap_presentation">
     <div class="parent_presentation">
-      <div class="child_photop"><img src="<?php echo(get_field('doctor_presentation')['url']); ?>"/></div>
+      <div class="child_photop"><img class="img_presentation" src="<?php echo(get_field('doctor_presentation')['url']); ?>"/></div>
       <div class='child_textp'><p class="mtitle_presentation"><?php the_field('title1'); ?></p> <p class="subtitle_presentation"><?php the_field('subtitle1'); ?> </p> <p><?php the_field('facialist'); ?></p></div>
 
     </div>
 
     <div class="parent_presentation">
-      <div class="child_photop2"><img src="<?php echo(get_field('massage_presentation')['url']); ?>"/></div>
+      <div class="child_photop2"><img class="img_presentation" src="<?php echo(get_field('massage_presentation')['url']); ?>"/></div>
       <div class="child_textp2"><p class="mtitle_presentation"><?php the_field('title2'); ?></p><p class="subtitle_presentation"><?php the_field('subtitle2'); ?> </p><p><?php the_field('faceup'); ?></p></div>
 
     </div>
 
     <div class="parent_presentation">
-      <div class="child_photop"><img src="<?php echo(get_field('face_presentation')['url']); ?>"/></div>
+      <div class="child_photop"><img class="img_presentation" src="<?php echo(get_field('face_presentation')['url']); ?>"/></div>
       <div class='child_textp'><p class="mtitle_presentation"><?php the_field('title3'); ?></p> <p class="subtitle_presentation"><?php the_field('subtitle3'); ?> </p> <p><?php the_field('face'); ?></p></div>
 
     </div>
