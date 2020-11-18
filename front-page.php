a8cc160cf431862548bc3d1dfcaef3e6ef397574
dutrieum <dutrieuxmathilde@gmail.com>
2020-11-18 13:21:33 +0100

    add image size

--- front-page.php
+++ front-page.php
@@ -7,7 +7,7 @@
 <main>
   <div class="main-carousel">
     <?php foreach( $images as $image ): ?>
-      <div class="carousel-cell"><img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['alt']); ?>"/></div>
+      <div class="carousel-cell"><img src="<?php echo wp_get_attachment_image( $image['url'], 'slider-size' ); ?>" alt="<?php echo esc_attr($image['alt']); ?>"/></div>
     <?php endforeach; ?>
   </div>
 </main>
