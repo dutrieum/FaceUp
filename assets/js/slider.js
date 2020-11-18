2b70ca73dda60d31e1806abe95499d7ac48cdf93
dutrieum <dutrieuxmathilde@gmail.com>
2020-11-18 11:59:44 +0100

    modif slider

---
+++ assets/js/slider.js
@@ -0,0 +1,8 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  autoPlay: true,
+  wrapAround: true
+});
--- front-page.php
+++ front-page.php
@@ -1,8 +1,15 @@
 <?php
   get_header();
+
+  $images = get_field('slider');
 ?>
 
 <main>
+  <div class="main-carousel">
+    <?php foreach( $images as $image ): ?>
+      <div class="carousel-cell"><img src="<?php echo esc_url($image['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($image['alt']); ?>"/></div>
+    <?php endforeach; ?>
+  </div>
 </main>
 
 <?php
