507e46bef724383aae2e865c52bc5c9b9dbd6d43
ruscae <rusca.emma@gmail.com>
2020-11-18 10:14:49 +0100

    Modif css footer font weight

--- templates/seances.php
+++ templates/seances.php
@@ -8,29 +8,29 @@
   get_header();
 ?>
 
-    <div id="title">
+    <div id="title_seances">
       <h1><?php the_field('title'); ?></h1>
     </div>
 
-    <div class="wrap">
-			<div class="parent">
-				<div class="child photo">
+    <div class="wrap seances">
+			<div class="parent seances">
+				<div class="child photo seances">
         <img src="<?php echo(get_field('picture1')['url']); ?>"/>
         </div>
-				<div class="child text">
+				<div class="child text seances">
           <h2><?php the_field('little_title1'); ?></h2>
           <p><?php the_field('text1'); ?></p>
         </div>
       </div>
-      <div class="parent">
-        <div class="child text">
+      <div class="parent seances">
+        <div class="child text seances">
           <h2><?php the_field('little_title2'); ?></h2>
           <p><?php the_field('text2'); ?></p>
         </div>
-				<div class="child photo">
+				<div class="child photo seances">
         <img src="<?php echo(get_field('picture2')['url']); ?>"/>
         </div>
-        <div class="child bouton">
+        <div class="child bouton seances">
         <p><?php $button = get_field('button');?></p>
         <a href="<?php echo(get_permalink($button->ID)); ?>">VOIR LES RÉSULTATS</a>
         </div>
