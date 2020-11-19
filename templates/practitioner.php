5d2ef1764e55b13779779aaf3de9edf4c1ff2ac5
ruscae <rusca.emma@gmail.com>
2020-11-19 11:19:12 +0100

    lien entre le php et le css de practitioner

---
+++ templates/practitioner.php
@@ -0,0 +1,18 @@
+<?php
+  /*
+    Template Name: practitioner
+  */
+?>
+
+<?php
+  get_header();
+?>
+
+  <div class="title_practitioner">
+    <h1><?php the_field('title_practitioner'); ?></h1>
+  </div>
+
+
+<?php
+  get_footer();
+?>
