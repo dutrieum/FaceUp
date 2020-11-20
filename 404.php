ac5fd3fc48eb0b646197b652a3f7f9db474e5a3c
ruscae <rusca.emma@gmail.com>
2020-11-20 10:18:59 +0100

    Page 404 modif

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php
+  /*
+    Template Name: 404
+  */
+?>
+<?php
+  get_header();
+?>
+
+  <div class="404">
+    <h1>404</h1>
+    <p>Cette page est introuvable</p>
+  </div>
+
+
+<?php
+  get_footer();
+?>
