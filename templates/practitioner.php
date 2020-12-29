cd150864473838618f6a57429e32083cd384421d
dutrieum <dutrieuxmathilde@gmail.com>
2020-12-29 17:35:32 +0100

    modifs practitioner

--- templates/practitioner.php
+++ templates/practitioner.php
@@ -12,8 +12,18 @@
   <div class="title_practitioner">
     <h1><?php the_field('title_practitioner'); ?></h1>
   </div>
-  <div class="map">
+  <!--  <div class="map">
     <iframe src="https://www.google.com/maps/d/embed?mid=1fikSmzQGUdQzE2qp2MZXXgEXHxlH9y-0" width="640" height="480"></iframe>
+  </div> -->
+  <div class="wrap_practitioner">
+    <div class="first_column_practitioner">
+      <h2><?php the_field('practitioner_first_subtitle'); ?></h2>
+      <p><?php the_field('practitioner_first_text'); ?></p>
+    </div>
+    <div class="first_column_practitioner">
+      <h2><?php the_field('practitioner_second_subtitle'); ?></h2>
+      <p><?php the_field('practitioner_second_text'); ?></p>
+    </div>
   </div>
   <div class="title_practitioner_prices">
     <p><?php the_field('title_practitioner_prices'); ?></p>
