2a8d46650f9be23b2c07db7e838b1ba8bae93c0a
dutrieum <dutrieuxmathilde@gmail.com>
2020-12-29 16:39:13 +0100

    modifs prices

--- templates/prices.php
+++ templates/prices.php
@@ -8,6 +8,15 @@ Template Name: Prices
   get_header();
 ?>
 
+<main>
+  <div class="wrap_prices_page">
+    <h1><?php the_field('prices_title'); ?></h1>
+    <div class="paragraph_prices">
+      <p><?php the_field('prices_text');?></p>
+    </div>
+  </div>
+</main>
+
 <?php
   get_footer();
 ?>
