7d880781635e0deafcbdd1a1b0dde38bfc36dde9
Mathilde <dutrieuxmathilde@gmail.com>
2021-02-25 19:02:05 +0100

    Menu responsive

--- functions.php
+++ functions.php
@@ -37,6 +37,7 @@
     wp_enqueue_style( 'need-responsive', get_template_directory_uri() . '/assets/css/need.responsive.css', array(), '1.1');
     wp_enqueue_style( 'job-responsive', get_template_directory_uri() . '/assets/css/job.responsive.css', array(), '1.1');
     wp_enqueue_style( 'practitioner-responsive', get_template_directory_uri() . '/assets/css/practitioner.responsive.css', array(), '1.1');
+    wp_enqueue_style( 'header-responsive', get_template_directory_uri() . '/assets/css/header.responsive.css', array(), '1.1');
 
 
 
